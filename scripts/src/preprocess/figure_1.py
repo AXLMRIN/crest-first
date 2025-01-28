8067b158b74e28919a4d8937caa4bb176d78a62a
AXLMRIN <axelmorinmail@gmail.com>
2025-01-28 16:06:08 +0100

    figure_1 preprocess - Working

---
+++ scripts/src/preprocess/figure_1.py
@@ -0,0 +1,128 @@
+'''
+What it does : 
+this file is meant to preprocess a data frame for the figure 1 : 
+graph : 
+    x : years
+    y : proportion (with or without rolling average)
+
+input : raw dataframe from EOLLION mail from the 2 week of January (mail) 
+    name : 2025-01-07-2024-10-29-quinquadef4-neat-abstract-bert.csv
+
+output : 
+    dataframe : 
+        columns :   year    | proportion   | label         | RA
+                    int     | float         | "SIC", ...    | bool
+    
+!! expected to change !! 
+'''
+# Third Parties
+import pandas as pd
+import numpy as np
+
+# Native
+
+# Custom 
+
+# Classes
+
+# Functions
+
+# Settings =====================================================================
+filepath : str = "data/"
+filename : str = "2025-01-07-2024-10-29-quinquadef4-neat-abstract-bert.csv"
+
+RA_window_size : int = 5
+
+filepath_save : str = "data/preprocessed/"
+filename_save : str = "figure_1.csv"
+
+# Open Files ===================================================================
+# >>> Only keep "bert_genre", "annee", "revue" 
+original_df : pd.DataFrame = pd.read_csv(
+    filepath + filename
+    ).loc[:,["annee", "revue", "bert_genre"]].dropna()
+
+# >>> Remove the years 2023 
+original_df.drop(
+    original_df[
+        original_df["annee"] == 2023
+    ].index, inplace = True)
+
+# Add the "discipline" column - - - - - - - - - - - - - - - - - - - - - - - - - 
+# >>> Open and clean discipline_per_revue
+discipline_per_revue : pd.DataFrame = pd.read_csv(
+    filepath + "Classification revues - Feuille 1.csv"
+    ).loc[:,["revue", "Dominante"]].dropna()
+ 
+discipline_per_revue["Dominante"] = discipline_per_revue["Dominante"].replace(
+    "Économie", "Economie")
+
+def what_discipline(revue : str):
+
+    try : 
+        to_return : str =  discipline_per_revue.loc[
+            discipline_per_revue["revue"] == revue,
+            "Dominante"
+        ].item()
+    except : 
+        to_return :str =  "<UNK>"
+    return to_return
+
+# >>> Add the column
+original_df["discipline"] = original_df["revue"].apply(what_discipline)
+
+# >>> Remove the rows where the discipline was not found
+print("- " * 50 + "\n",
+      "Discipline not found for : \n\n",
+      ''.join([
+    el + ', ' for el in set(original_df.loc[
+                                original_df["discipline"] == "<UNK>",
+                                "revue"])]) + '\n',
+    "- " * 50 + "\n")
+
+original_df.drop(
+    original_df.loc[
+        original_df["discipline"] == "<UNK>",:].index,
+    axis = 0, inplace = True)
+# Evaluate the proportion - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# >>> Group the dataframe by discipline
+grouped_df = original_df.groupby("discipline")
+
+# NOTE there might be a cleanier way of doing it 
+year_set = set(original_df["annee"]) # is sorted
+new_df = []
+for discipline, discipline_df in grouped_df : 
+    for year in year_set:
+        new_df.append({
+            "RA" : False,
+            "annee" : year,
+            "discipline" : discipline,
+            "proportion" : discipline_df.loc[
+                discipline_df["annee"] == year,
+                "bert_genre"].mean()
+        })
+
+# Proceed to the Rolling Average - - - - - - - - - - - - - - - - - - - - - - - -
+window : np.ndarray = np.ones(RA_window_size) / RA_window_size
+years_RA : list = list(year_set)[RA_window_size - 1:]
+
+for discipline, discipline_df in grouped_df :
+    RA : np.ndarray = np.convolve(
+        discipline_df["bert_genre"], window,
+        mode = "valid")
+    
+    for proportion, year in zip(RA, years_RA): 
+        new_df.append({
+            "RA" : True,
+            "annee" : year,
+            "discipline" : discipline,
+            "proportion" : proportion
+        })
+
+# Save to csv - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+pd.DataFrame(new_df).to_csv(
+    filepath_save + filename_save,
+    index = False
+)
+# ==============================================================================
+
