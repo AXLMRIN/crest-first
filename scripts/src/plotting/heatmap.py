3cbc76f3fb48347b9c8a842633fb61851802365c
AXLMRIN <axelmorinmail@gmail.com>
2025-02-03 19:32:01 +0100

    version primitive - running

--- scripts/src/plotting/heatmap.py
+++ scripts/src/plotting/heatmap.py
@@ -14,19 +14,67 @@ sys.path.append(("/Users/axelmorin/Library/Mobile Documents/com~apple~CloudDocs"
 # Classes
 
 # Functions
+from package.heatmap import (
+    make_xyz,
+    add_heatmap
+)
 
 # Settings =====================================================================
-
 filenames = {
     "open" : "figure_1_per_revue.csv",
     "save" : "heatmap.html"
 }
+
+jsonfiles = {}
+
 # Open files ====================================================================
+OPENPATH  = "data/preprocessed/"
+df_plot = pd.read_csv(OPENPATH + filenames["open"])
+df_plot = df_plot.loc[
+    df_plot["RA"] == False, ["annee","revue","discipline","proportion"]
+]
+df_plot.index = range(len(df_plot))
 
 # Create the figure =============================================================
-fig = go.Figure()
+fig = go.Figure(
+    layout = {
+        'xaxis':  {'anchor': 'y', 'domain': [0.0, 1.0]},
+        'yaxis':  {'anchor': 'x', 'domain': [0, 1.0]}
+    }
+)
+# 'xaxis2': {'anchor': 'y2', 'domain': [0.0, 1.0]},
+# 'yaxis2': {'anchor': 'x2', 'domain': [0.0, 0.425]}
 
+# Create the heatmap - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# TODO Create for all disciplines
+year_array = list(set(df_plot["annee"]))
+grouped = df_plot.groupby("discipline")
+sub_df = grouped.get_group("Sociologie")
+revue_array = sorted(list(set(sub_df["revue"])), reverse = True)
 
+z = [
+    sub_df.loc[sub_df["revue"] == revue,"proportion"].to_list()
+    for revue in revue_array
+]
+# --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --
+
+colorscale=[
+        [0, "rgb(255,255,255)"],
+        [0.5, "rgb(255,0,0)"],
+        [1.0, "rgb(0,0,0)"],
+    ]
+colorbar={
+    "tickvals" : [0,50,100,1500],
+    "ticksuffix" : " %"
+}
+
+fig.add_trace(
+    go.Heatmap(x = year_array, y = revue_array, z = z,
+               zmid = 50, colorbar = colorbar, colorscale = colorscale,
+               visible = True)
+)
+
+print(fig.data[0])
 
 # Save the figure - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 SAVEPATH = "views/"
