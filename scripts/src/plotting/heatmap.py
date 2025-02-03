69eb026950e429db344a69801a1c491c2143f33c
AXLMRIN <axelmorinmail@gmail.com>
2025-02-03 09:52:37 +0100

    Created the heatmap file

---
+++ scripts/src/plotting/heatmap.py
@@ -0,0 +1,17 @@
+# Third Parties
+
+
+# Native
+# NOTE might be worth finding a cleaner way to do that
+import sys
+sys.path.append(("/Users/axelmorin/Library/Mobile Documents/com~apple~CloudDocs"
+                 "/Axel_tout/Professionnel/Stages/TFE/CREST/workdirectory/Genre"
+                 "/dataVis/plotly-datavis-crest/scripts/src"))
+
+# Custom 
+
+# Classes
+
+# Functions
+
+# Settings =====================================================================
