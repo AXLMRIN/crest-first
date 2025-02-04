2e69e4e805c80b75f1e078ed68e96d039cbad7b0
AXLMRIN <axelmorinmail@gmail.com>
2025-02-04 11:23:01 +0100

    created the make_xyz function - running

--- scripts/src/package/heatmap/data_handler.py
+++ scripts/src/package/heatmap/data_handler.py
@@ -13,23 +13,15 @@ import numpy as np
 
 # Export functions =============================================================
 
-def make_xyz(df : pd.DataFrame, x_col : str, y_col : str, z_col : str
-             ) -> tuple[np.ndarray, np.ndarray, np.ndarray] : 
+def make_xyz(df : pd.DataFrame) -> tuple[list, list, list] : 
     """Takes a dataframe and 3 columns. Makes it into a 2d array
     Both axis must be sorted
     """
+    year_array = list(set(df["annee"]))
+    revue_array = sorted(list(set(df["revue"])), reverse = True)
 
-    x_set = set(df[x_col])
-    y_set = set(df[y_col])
-    
-    z_output = np.zeros([len(x_set), len(y_set)])
-    for i, x in enumerate(x_set) :
-        for j,y in enumerate(y_set) : 
-            try : 
-                item = df.loc[
-                    (df[x_col] == x) & (df[y_col] == y),
-                    z_col].item()
-            except : 
-                item = np.nan
-            z_output[i,j] = item
-    return np.array(x), np.array(y), z_output
+    z = [
+        df.loc[df["revue"] == revue,"proportion"].to_list()
+        for revue in revue_array
+    ]
+    return year_array, revue_array, z
