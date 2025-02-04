dc7d24708579b2cfa9a394009dec2af259ef803c
AXLMRIN <axelmorinmail@gmail.com>
2025-02-04 11:23:44 +0100

    created all data - running

--- scripts/src/plotting/heatmap.py
+++ scripts/src/plotting/heatmap.py
@@ -18,63 +18,108 @@ from package.heatmap import (
     make_xyz,
     add_heatmap
 )
+from package.data_loader import (
+    load_JSON_parameters
+)
 
 # Settings =====================================================================
 filenames = {
-    "open" : "figure_1_per_revue.csv",
+    "open_per_revue" : "figure_1_per_revue.csv",
+    "open_per_discipline" : "figure_1_per_discipline.csv",
     "save" : "heatmap.html"
 }
-
-jsonfiles = {}
+# TODO Think of a better way to manage the settings 
+jsonfiles = {
+    "general" : "general_theme.json",
+    "xaxis" : "xaxis.json",
+    "yaxis" : "yaxis.json",
+    "legend" : "legend.json"
+}
 
 # Open files ====================================================================
 OPENPATH  = "data/preprocessed/"
-df_plot = pd.read_csv(OPENPATH + filenames["open"])
-df_plot = df_plot.loc[
-    df_plot["RA"] == False, ["annee","revue","discipline","proportion"]
+df_plot_per_revue = pd.read_csv(OPENPATH + filenames["open_per_revue"])
+df_plot_per_revue = df_plot_per_revue.loc[
+    df_plot_per_revue["RA"] == False, ["annee","revue","discipline","proportion"]
 ]
-df_plot.index = range(len(df_plot))
+df_plot_per_revue.index = range(len(df_plot_per_revue))
+
+df_plot_per_discipline = pd.read_csv(OPENPATH + filenames["open_per_discipline"])
+df_plot_per_discipline = df_plot_per_discipline.loc[
+    df_plot_per_discipline["RA"] == True, ["annee", "discipline", "proportion"]
+]
+df_plot_per_discipline.index = range(len(df_plot_per_discipline))
+
+parameters = load_JSON_parameters(jsonfiles)
 
 # Create the figure =============================================================
 fig = go.Figure(
     layout = {
-        'xaxis':  {'anchor': 'y', 'domain': [0.0, 1.0]},
-        'yaxis':  {'anchor': 'x', 'domain': [0, 1.0]}
+        'xaxis':  {'anchor': 'x' , 'domain': [0.0, 1.0  ]},
+        'yaxis':  {'anchor': 'y' , 'domain': [0.0, 0.25 ]},
+        'yaxis2': {'anchor': 'y2', 'domain': [0.3, 1.0  ]},
     }
 )
-# 'xaxis2': {'anchor': 'y2', 'domain': [0.0, 1.0]},
-# 'yaxis2': {'anchor': 'x2', 'domain': [0.0, 0.425]}
+
+
+# Customise figure general parameters - - - - - - - - - - - - - - - - - - - - - -
+fig.update_layout(
+    paper_bgcolor = parameters["general"]["theme-colors"]["primary"],
+    plot_bgcolor = parameters["general"]["theme-colors"]["primary"], 
+    height = 800, width = 1200
+)
+
+# Customise axis - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+fig.update_layout(
+    xaxis = parameters["xaxis"],
+    yaxis = parameters["yaxis"],
+    yaxis2 = parameters["yaxis"]
+)
+
+# customise the legend - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+fig.update_layout(
+    legend = parameters["legend"]
+)
+fig.update_layout(
+    legend = dict(
+        x = 0.5,
+        y = -0.1
+    )
+)
 
 # Create the heatmap - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# TODO Create for all disciplines
-year_array = list(set(df_plot["annee"]))
-grouped = df_plot.groupby("discipline")
-sub_df = grouped.get_group("Sociologie")
-revue_array = sorted(list(set(sub_df["revue"])), reverse = True)
+trace_bind = {}
+for discipline, sub_df in df_plot_per_revue.groupby("discipline") : 
+    annee_vec, revue_vec, proportion_matrix = make_xyz(sub_df)
+    add_heatmap(fig, x = annee_vec, y = revue_vec, z = proportion_matrix)
+    trace_bind[discipline + "_heatmap"] = len(fig.data) - 1
 
-z = [
-    sub_df.loc[sub_df["revue"] == revue,"proportion"].to_list()
-    for revue in revue_array
-]
-# --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --
 
-colorscale=[
-        [0, "rgb(255,255,255)"],
-        [0.5, "rgb(255,0,0)"],
-        [1.0, "rgb(0,0,0)"],
-    ]
-colorbar={
-    "tickvals" : [0,50,100,1500],
-    "ticksuffix" : " %"
-}
+# Create the curve - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+grouped_discipline = df_plot_per_discipline.groupby("discipline")
+sub_df = grouped_discipline.get_group("Sociologie")
+for discipline, sub_df in df_plot_per_discipline.groupby("discipline") :
+    if discipline == "Toutes" : pass
+    fig.add_trace(
+        go.Scatter(x = sub_df["annee"], y = sub_df["proportion"],
+                xaxis = "x", yaxis = "y", mode = "lines", 
+                name = discipline + ", moyenne des revues",
+                visible = False)
+    )
+    trace_bind[discipline + "_trace"] = len(fig.data) - 1
 
+
+sub_df = grouped_discipline.get_group("Toutes")
 fig.add_trace(
-    go.Heatmap(x = year_array, y = revue_array, z = z,
-               zmid = 50, colorbar = colorbar, colorscale = colorscale,
+    go.Scatter(x = sub_df["annee"], y = sub_df["proportion"],
+               xaxis = "x", yaxis = "y", mode = "lines",
+               name = "Moyenne de toutes les disciplines", 
                visible = True)
 )
 
-print(fig.data[0])
+# Display one discipline - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+fig.data[trace_bind["Anthropologie_heatmap"]].visible = True
+fig.data[trace_bind["Anthropologie_trace"]].visible = True
 
 # Save the figure - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 SAVEPATH = "views/"
