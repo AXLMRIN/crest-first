5ec4498e220a9818180b198600fb196b19fbf0d2
AXLMRIN <axelmorinmail@gmail.com>
2025-02-04 14:57:18 +0100

    major upgrades, everything works except colorscale

--- scripts/src/plotting/heatmap.py
+++ scripts/src/plotting/heatmap.py
@@ -16,7 +16,9 @@ sys.path.append(("/Users/axelmorin/Library/Mobile Documents/com~apple~CloudDocs"
 # Functions
 from package.heatmap import (
     make_xyz,
-    add_heatmap
+    add_heatmap,
+    add_menu,
+    sort_revue_and_matrix
 )
 from package.data_loader import (
     load_JSON_parameters
@@ -66,7 +68,8 @@ fig = go.Figure(
 fig.update_layout(
     paper_bgcolor = parameters["general"]["theme-colors"]["primary"],
     plot_bgcolor = parameters["general"]["theme-colors"]["primary"], 
-    height = 800, width = 1200
+    height = 800, width = 1200,
+    margin=dict(l=200, r=200),
 )
 
 # Customise axis - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -87,39 +90,53 @@ fig.update_layout(
     )
 )
 
-# Create the heatmap - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Create the heatmaps - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 trace_bind = {}
 for discipline, sub_df in df_plot_per_revue.groupby("discipline") : 
     annee_vec, revue_vec, proportion_matrix = make_xyz(sub_df)
-    add_heatmap(fig, x = annee_vec, y = revue_vec, z = proportion_matrix)
+    revue_vec, proportion_matrix = sort_revue_and_matrix(revue_vec, proportion_matrix)
+    add_heatmap(fig, x = annee_vec, y = revue_vec, z = proportion_matrix, discipline = discipline)
     trace_bind[discipline + "_heatmap"] = len(fig.data) - 1
 
 
-# Create the curve - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Create the curves - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 grouped_discipline = df_plot_per_discipline.groupby("discipline")
-sub_df = grouped_discipline.get_group("Sociologie")
+
+sub_df = grouped_discipline.get_group("Toutes")
+fig.add_trace(
+    go.Scatter( x = sub_df["annee"], y = sub_df["proportion"],
+                xaxis = "x", yaxis = "y", mode = "lines",
+                name = "Moyenne de toutes les disciplines", 
+                line = dict(
+                   color = "black", dash = "longdash"
+                ),
+                visible = True)
+)
+trace_bind["Toutes_trace"] = len(fig.data) - 1
+
 for discipline, sub_df in df_plot_per_discipline.groupby("discipline") :
-    if discipline == "Toutes" : pass
+    if discipline == "Toutes" : continue
     fig.add_trace(
         go.Scatter(x = sub_df["annee"], y = sub_df["proportion"],
                 xaxis = "x", yaxis = "y", mode = "lines", 
                 name = discipline + ", moyenne des revues",
+                line = dict(color = "black" ),
                 visible = False)
     )
     trace_bind[discipline + "_trace"] = len(fig.data) - 1
 
 
-sub_df = grouped_discipline.get_group("Toutes")
-fig.add_trace(
-    go.Scatter(x = sub_df["annee"], y = sub_df["proportion"],
-               xaxis = "x", yaxis = "y", mode = "lines",
-               name = "Moyenne de toutes les disciplines", 
-               visible = True)
-)
 
 # Display one discipline - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-fig.data[trace_bind["Anthropologie_heatmap"]].visible = True
-fig.data[trace_bind["Anthropologie_trace"]].visible = True
+
+
+# Create the menu - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+discipline_set = sorted(list(set(df_plot_per_revue["discipline"])))
+domain_sizes = [0.7, 0.5875, 0.3, 0.925, 0.625, 0.8125, 0.7375, 0.4375, 0.7375, 0.5875, 0.3]
+
+add_menu(fig, trace_bind, discipline_set, domain_sizes)
+
+# Set one facet active - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 # Save the figure - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 SAVEPATH = "views/"
