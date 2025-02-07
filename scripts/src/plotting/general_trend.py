f59c4f2036771a06377c05771e98fb474ceccb2f
AXLMRIN <axelmorinmail@gmail.com>
2025-02-07 14:28:09 +0100

    kinda reached the end ... - running

--- scripts/src/plotting/general_trend.py
+++ scripts/src/plotting/general_trend.py
@@ -26,8 +26,13 @@ filenames = {
     "save" : "general_trend.html"
 }
 
-theme = GeneralTheme(**{})
+discipline_to_NOT_show = set(["Toutes", "Aréale", "Autre interdisciplinaire"])
 
+theme = GeneralTheme(**{
+    "xaxis" : {"title" : ""},
+    "yaxis" : {"title" : ""}
+})
+eps = 0.025
 # Open Files ===================================================================
 selected_columns = ["annee", "discipline", "proportion", "text"]
 
@@ -36,54 +41,58 @@ df_plot = pd.read_csv(OPENPATH + filenames["open_per_revue"])
 df_plot = df_plot.loc[df_plot["RA"] == True, selected_columns]
 df_plot.index = range(len(df_plot))
 
+
+DISCIPLINE_SET = sorted(list(
+    set(df_plot["discipline"]) - discipline_to_NOT_show
+))
+n_disciplines : int  = len(DISCIPLINE_SET)
 # Create the figure =============================================================
 # TODO Refactor
-eps = 0.01
-fig = go.Figure(
-    layout = {
-        'xaxis'   :  {'anchor': 'y'   , 'domain': [0.0, 1.0  ]},
-        'xaxis2'  :  {'anchor': 'y2'  , 'domain': [0.0, 1.0  ]},
-        'xaxis3'  :  {'anchor': 'y3'  , 'domain': [0.0, 1.0  ]},
-        'xaxis4'  :  {'anchor': 'y4'  , 'domain': [0.0, 1.0  ]},
-        'xaxis5'  :  {'anchor': 'y5'  , 'domain': [0.0, 1.0  ]},
-        'xaxis6'  :  {'anchor': 'y6'  , 'domain': [0.0, 1.0  ]},
-        'xaxis7'  :  {'anchor': 'y7'  , 'domain': [0.0, 1.0  ]},
-        'xaxis8'  :  {'anchor': 'y8'  , 'domain': [0.0, 1.0  ]},
-        'xaxis9'  :  {'anchor': 'y9'  , 'domain': [0.0, 1.0  ]},
-        'xaxis10' :  {'anchor': 'y10' , 'domain': [0.0, 1.0  ]},
-        'xaxis11' :  {'anchor': 'y11' , 'domain': [0.0, 1.0  ]},
-        'yaxis'  :  {'anchor': 'y' , 'domain': [0.80 + eps, 1.0 - eps ]},
-        'yaxis2' : {'anchor': 'y2' , 'domain': [0.72 + eps, 0.80 - eps]},
-        'yaxis3' : {'anchor': 'y3' , 'domain': [0.64 + eps, 0.72 - eps]},
-        'yaxis4' : {'anchor': 'y4' , 'domain': [0.56 + eps, 0.64 - eps]},
-        'yaxis5' : {'anchor': 'y5' , 'domain': [0.48 + eps, 0.56 - eps]},
-        'yaxis6' : {'anchor': 'y6' , 'domain': [0.40 + eps, 0.48 - eps]},
-        'yaxis7' : {'anchor': 'y7' , 'domain': [0.32 + eps, 0.40 - eps]},
-        'yaxis8' : {'anchor': 'y8' , 'domain': [0.24 + eps, 0.32 - eps]},
-        'yaxis9' : {'anchor': 'y9' , 'domain': [0.16 + eps, 0.24 - eps]},
-        'yaxis10': {'anchor': 'y10', 'domain': [0.08 + eps, 0.16 - eps]},
-        'yaxis11': {'anchor': 'y11', 'domain': [0.00 + eps, 0.08 - eps]},
-        "hoversubplots" : "axis",
+fig = go.Figure( layout = {
+    **{
+        "xaxis" : {"anchor" : "y", "domain" : [0.0,1]},
+        "yaxis" : {"anchor" : "y", "domain" : [0.8,1]}
+    },
+    **{
+        f"yaxis{i+2}" : {
+            'anchor': f'y{i+2}'  ,
+            'domain': [i * (0.8 / n_disciplines) + eps, 
+                       (i + 1) * (0.8 / n_disciplines) - eps]}
+        for i in range(n_disciplines)
+    },
+    **{
+        f"xaxis{i+2}" : {'anchor': f'y{i+2}'  , 'domain': [0.0, 1.0  ]}
+        for i in range(n_disciplines)
     }
-)
+})
+
+theme.xaxis.config["showgrid"] = False
+theme.yaxis.config["showgrid"] = False
+
+fig.update_layout({
+    "xaxis" : theme.xaxis.config,
+    "yaxis" : theme.xaxis.config,
+    **{
+        f"xaxis{i + 2}" : theme.xaxis.config for i in range(n_disciplines)
+    },
+    **{
+        f"yaxis{i + 2}" : theme.yaxis.config for i in range(n_disciplines)
+    }
+})
 
 fig.update_layout(
     paper_bgcolor = theme.primary_color,
     plot_bgcolor = theme.primary_color, 
-    height = 1500, width = 1000,
+    height = 1800, width = 1000,
     margin=dict(l=50, r=50),
 )
 
 fig.update_layout(
     legend = {"visible" : False},
-    hoverlabel= {"bgcolor" : theme.primary_color}
+    hoverlabel= {"bgcolor" : theme.tertiary_colour}
 )
 
 # Add the traces - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-DISCIPLINE_SET = sorted(list(
-                    set(df_plot["discipline"]) - set(["Toutes", "Autre interdisciplinaire"])
-                ))
-print(DISCIPLINE_SET)
 df_grouped = df_plot.groupby("discipline")
 for i, discipline in enumerate(DISCIPLINE_SET) : 
 
