23dfea468c544a3be94801eddea61a39bb21bf82
AXLMRIN <axelmorinmail@gmail.com>
2025-02-07 09:30:26 +0100

    initial commit

---
+++ scripts/src/plotting/general_trend.py
@@ -0,0 +1,41 @@
+# Third Parties
+import plotly.graph_objects as go
+import pandas as pd
+
+# Native
+# NOTE might be worth finding a cleaner way to do that
+import sys
+sys.path.append(("/Users/axelmorin/Library/Mobile Documents/com~apple~CloudDocs"
+                 "/Axel_tout/Professionnel/Stages/TFE/CREST/workdirectory/Genre"
+                 "/dataVis/plotly-datavis-crest/scripts/src"))
+sys.path.append(("/Users/axelmorin/Library/Mobile Documents/com~apple~CloudDocs"
+                 "/Axel_tout/Professionnel/Stages/TFE/CREST/workdirectory/Genre"
+                 "/dataVis/plotly-datavis-crest/scripts"))
+
+# Custom 
+
+# Classes
+from plotlyThemes.general_theme import GeneralTheme
+
+# Functions
+
+
+# Settings =====================================================================
+filenames = {
+    "open_per_revue" : "prop_genre_classe_per_revue.csv",
+    "save" : "scatter_plot.html"
+}
+
+theme = GeneralTheme(**{
+    "xaxis" : {"title" : "Proportion des articles mentionnant la classe"},
+    "yaxis" : {"title" : "Proportion des articles mentionnant le genre"}
+})
+
+# Open Files ===================================================================
+selected_columns = ["revue", "discipline", "annee","proportion_genre",
+                    "proportion_classe", "n_articles", "text"]
+
+OPENPATH  = "data/preprocessed/"
+df_plot = pd.read_csv(OPENPATH + filenames["open_per_revue"])
+df_plot = df_plot.loc[df_plot["RA"] == True, selected_columns]
+df_plot.index = range(len(df_plot))
