d87d70e8fbc43b0149efc9501c4859b4df812301
AXLMRIN <axelmorinmail@gmail.com>
2025-02-10 17:20:43 +0100

    initial commit

---
+++ scripts/src/package/auteurice/data_plotter.py
@@ -0,0 +1,13 @@
+# Third Parties
+import plotly.graph_objects as go
+import pandas as pd
+
+# Native
+
+# Custom 
+
+# Classes
+
+# Functions
+
+# ==============================================================================
