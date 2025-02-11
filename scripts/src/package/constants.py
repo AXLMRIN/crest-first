dbf84c4396c4a4b3f65775450c541cabd7b2fcae
AXLMRIN <axelmorinmail@gmail.com>
2025-02-11 16:50:22 +0100

    cleant the hole shit (local archive)

--- scripts/src/package/constants.py
+++
@@ -1,7 +0,0 @@
-DATADIRPATH = DATADIRPATH     = "../../data/checkpoints/"
-SAVEPATH    = DATADIRPATH + "checkpoints/"
-
-PLOTLYARGS = ['color', 'line_dash', 'symbol', 'hover_name', 'hover_data',
-              'facet_row', 'facet_col', 'error_x', 'error_y', 'error_x_minus',
-              'error_y_minus', 'animation_frame', 'animation_group']
-# 'text' for annotation
