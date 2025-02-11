1c54cb8ba7631fadc9b908257363a601a8ba0f6a
AXLMRIN <axelmorinmail@gmail.com>
2025-02-11 08:30:56 +0100

    customised colors and ticks

--- scripts/src/plotting/auteurices.py
+++ scripts/src/plotting/auteurices.py
@@ -35,6 +35,7 @@ bin_size : float = 0.05
 
 # TODO Think of a better way to manage the settings
 theme = GeneralTheme(**{
+    "xaxis" : {"title" : "", "grid_opacity" : 0.7},
     "yaxis" : {"title" : ""}
 })
 
@@ -54,10 +55,12 @@ fig.update_layout(
     paper_bgcolor = theme.primary_color,
     plot_bgcolor = theme.primary_color, 
     height = 800, width = 1200,
-    margin=dict(l=200, r=200),
+    margin=dict(l=20, r=20),
 )
 
 # Create abstract axis =========================================================
+theme.xaxis.config["showline"] = False
+theme.xaxis.config["range"] = [-125, 125]
 theme.xaxis.config["zeroline"] = False
 theme.xaxis.config["tickvals"] = [ 
     int(- middle_gap - 50), int(- middle_gap - 25),
@@ -75,13 +78,55 @@ fig.update_layout(
     yaxis = theme.yaxis.config
 )
 
+# Add the ticks - Alias does'nt seem to work well - - - - - - - - - - - - - - - 
+def label_transform(x) : 
+    """
+    even with xref, yref being "paper", the (0,0) point points to the 
+    bottom left corner of the plot area and (1,1) to the top right corner
+    """
+    dx1, dx2 = (abs(el) for el in theme.xaxis.config["range"])
+    return (x + dx1) / (dx1 + dx2)
+
+alias = {
+    int(- middle_gap - 50) : "50 %",
+    int(- middle_gap - 25) : "25 %",
+    int(  middle_gap + 25) : "25 %",
+    int(  middle_gap + 50) : "50 %"
+}
+
+for x in alias : 
+    fig.add_annotation(
+        text = alias[x],
+        x = label_transform(x), y = -0.05,
+        xref = "paper", yref = "paper",
+        showarrow = False, 
+        xanchor = "center", align = "center"
+    )
+
+fig.add_annotation(
+    text = "Femme (présumé)",
+    x = 0, y = 0.5,
+    textangle= -90,
+    xref = "paper", yref = "paper",
+    showarrow = False, 
+    xanchor = "center", align = "center"
+)
+
+fig.add_annotation(
+    text = "Homme (présumé)",
+    x = 1, y = 0.5,
+    textangle= 90,
+    xref = "paper", yref = "paper",
+    showarrow = False, 
+    xanchor = "center", align = "center"
+)
 # Create Rectangles ============================================================
 idx = np.argsort(df_plot["r_w"])
 disc_sorted = np.array(df_plot["discipline"])[idx]
 
 
 n_disciplines = len(df_plot)
-y_tick_value = 0.9 / n_disciplines
+y_tick_value = 1 / n_disciplines
 
 grouped = df_plot.groupby("discipline")
 
@@ -91,36 +136,40 @@ for i, discipline in enumerate(disc_sorted):
     fig.add_trace(
         make_a_bar( - middle_gap, y_tick_value * i + 0.05,
                 -1 * discipline_df["r_w"].item(), bin_size,
-                name = discipline, fillcolor = "pink",
-                line = {"color" : "pink"},
-                showlegend = False)
+                name = "a publié", fillcolor = "rgba(180,180,180,0.7)",
+                line = {"color" : "black", "width" : 1},
+                # legendgroup = "a publié",
+                showlegend = True)
     )
     
     # Add women rates specialised in gender
     fig.add_trace(
         make_a_bar( - middle_gap, y_tick_value * i + 0.05,
                 -1 * discipline_df["r_w_g"].item(), bin_size,
-                name = discipline, fillcolor = "red",
-                line = {"color" : "red"},
-                showlegend = False)
+                name = "a publié sur le genre", fillcolor = "rgba(100,100,100,0.7)",
+                line = {"color" : "black", "width" : 1},
+                # legendgroup = "a publié sur le genre",
+                showlegend = True)
     )
     
     # Add men rates
     fig.add_trace(
         make_a_bar( middle_gap, y_tick_value * i + 0.05,
                 discipline_df["r_m"].item(), bin_size,
-                name = discipline, fillcolor = "blue",
-                line = {"color" : "blue"},
-                showlegend = False)
+                name = "a publié", fillcolor = "rgba(180,180,180,0.7)",
+                line = {"color" : "black", "width" : 1},
+                # legendgroup = "a publié",
+                showlegend = True)
     )
     
     # Add women rates specialised in gender
     fig.add_trace(
         make_a_bar( middle_gap, y_tick_value * i + 0.05,
                 discipline_df["r_m_g"].item(), bin_size,
-                name = discipline, fillcolor = "green",
-                line = {"color" : "green"},
-                showlegend = False)
+                name = "a publié sur le genre", fillcolor = "rgba(100,100,100,0.7)",
+                line = {"color" : "black", "width" : 1},
+                # legendgroup = "a publié sur le genre",
+                showlegend = True)
     )
 
     # Add the discipline name 
