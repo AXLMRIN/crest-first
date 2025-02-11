865e7e48b83cd456a7768a950c11dbe43cffb824
AXLMRIN <axelmorinmail@gmail.com>
2025-02-11 17:14:05 +0100

    added parameters for auteurices

---
+++ scripts/src/package/__init__.py
@@ -0,0 +1,30 @@
+
+figure_parameters : dict = {
+    'auteurices' : {
+        "general" : {
+            "height" : 800,
+            "height" : 1200,
+            "margin" : {"l" : 20, "r" : 20}
+        },
+        "xaxis" : {
+            "title" : "",
+            "grid_opacity" : 0.7,
+            "showline" : False,
+            "range" : [-125, 125],
+            "zeroline" : False,
+            "showticklabels" : False
+        },
+        "yaxis" : {
+            "title" : "",
+            "showgrid" : False,
+            "zeroline" : False,
+            "showticklabels" : False,
+            "tickvals" : []
+        },
+        "legend" : {
+            "position" : (0.5, 1.05),
+            "itemclick" : False,
+            "itemdoubleclick" : False
+        }
+    }
+}
