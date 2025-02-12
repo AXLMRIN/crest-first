8e22ab849baf755f2bd52aecdc407fc54d0f5e7b
AXLMRIN <axelmorinmail@gmail.com>
2025-02-12 11:48:38 +0100

    created Heatmap object

---
+++ scripts/src/package/heatmap/Heatmap.py
@@ -0,0 +1,134 @@
+import pandas as pd
+import plotly.graph_objects as go
+from copy import deepcopy
+
+from .. import GeneralTheme
+from .data_plotter import (
+    add_heatmap,
+    add_menu,
+)
+from .data_handler import(
+    make_xyz,
+    sort_revue_and_matrix
+)
+
+class Heatmap : 
+    def __init__(self,
+        filename_open : str,
+        filename_save : str,
+        alternative_config : dict = {}) :
+        # TODO change the openfile function
+        # Open files ============================================================
+        OPENPATH  = "data/preprocessed/"
+        df_plot_per_revue = pd.read_csv(
+            OPENPATH + filename_open["open_per_revue"]
+        )
+        df_plot_per_revue = df_plot_per_revue.loc[
+            (df_plot_per_revue["RA"] == False) &
+            (df_plot_per_revue["definition"] == "restr"),
+            ["annee","revue","discipline","proportion"]
+        ]
+        df_plot_per_revue.index = range(len(df_plot_per_revue))
+
+        df_plot_per_discipline = pd.read_csv(
+            OPENPATH + filename_open["open_per_discipline"]
+        )
+        df_plot_per_discipline = df_plot_per_discipline.loc[
+            (df_plot_per_discipline["RA"] == True) & 
+            (df_plot_per_discipline["definition"] == "restr"),
+            ["annee", "discipline", "proportion"]
+        ]
+        df_plot_per_discipline.index = range(len(df_plot_per_discipline))
+
+        # Load the theme
+        theme = GeneralTheme()
+        theme.change_config(alternative_config)
+
+        # Create the 
+        fig = go.Figure(layout = {
+            "paper_bgcolor" : theme.primary_color,
+            "plot_bgcolor"  : theme.primary_color
+        })
+
+        if "general" in alternative_config : 
+            fig.update_layout(alternative_config["general"])
+        
+        theme2 = deepcopy(theme)
+        theme2.yaxis.config["title"]["text"] = "Revue"
+
+        fig.update_layout(
+            xaxis  = theme.xaxis.config,
+            yaxis  = theme.yaxis.config,
+            yaxis2 = theme2.yaxis.config,
+            legend = theme.legend.config
+        )
+
+        # Create the heatmaps - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+        trace_bind = {}
+        for discipline, sub_df in df_plot_per_revue.groupby("discipline") : 
+            annee_vec, revue_vec, proportion_matrix = make_xyz(sub_df)
+            revue_vec, proportion_matrix = sort_revue_and_matrix(revue_vec, proportion_matrix)
+            add_heatmap(fig, annees = annee_vec, revue_names = revue_vec, 
+                        proportions = proportion_matrix, discipline = discipline)
+            trace_bind[discipline + "_heatmap"] = len(fig.data) - 1
+
+        # Create the curves - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+        grouped_discipline = df_plot_per_discipline.groupby("discipline")
+
+        sub_df = grouped_discipline.get_group("Toutes")
+        fig.add_trace(
+            go.Scatter( x = sub_df["annee"], y = sub_df["proportion"],
+                        xaxis = "x", yaxis = "y", mode = "lines+markers",
+                        name = "Moyenne de toutes les disciplines", 
+                        line = dict(
+                        color = "rgb(194,6,26)"
+                        ),
+                        marker = dict(size = 4),
+                        hovertemplate="<b>%{x}</b><br>%{y:.1f} %",
+                        visible = True)
+        )
+        trace_bind["Toutes_trace"] = len(fig.data) - 1
+
+        for discipline, sub_df in df_plot_per_discipline.groupby("discipline") :
+            if discipline == "Toutes" : continue
+            fig.add_trace(
+                go.Scatter(x = sub_df["annee"], y = sub_df["proportion"],
+                        xaxis = "x", yaxis = "y", mode = "lines+markers", 
+                        name = discipline + ", moyenne des revues",
+                        # line = dict(color = theme.traces_color[discipline] ),
+                        line = dict(color = "rgb(65, 115, 185)" ),
+                        marker = dict(size = 4),
+                        hovertemplate="<b>%{x}</b><br>%{y:.1f} %",
+                        visible = False)
+            )
+            trace_bind[discipline + "_trace"] = len(fig.data) - 1
+
+        # Display one discipline - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+        # Create the menu - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+        discipline_sizes = {
+            'Anthropologie'             : 0.72,
+            'Aréale'                    : 0.615,
+            'Autre interdisciplinaire'  : 0.3, 
+            'Démographie'               : 0.93, 
+            'Économie'                  : 0.65, 
+            'Genre'                     : 0.825, 
+            'Géographie'                : 0.755, 
+            'Histoire'                  : 0.475, 
+            'SIC'                       : 0.755, 
+            'Science politique'         : 0.615, 
+            'Sociologie'                : 0.3
+        }
+
+        add_menu(fig, trace_bind, discipline_sizes, theme2.yaxis)
+
+        # Set one facet active - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        fig.data[trace_bind["Anthropologie_heatmap"]].visible = True
+        fig.data[trace_bind["Anthropologie_trace"]].visible = True
+
+        # Save the figure - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        SAVEPATH = "views/"
+        # NOTE change 'include_plotlyjs' for lighter files
+        fig.write_html(SAVEPATH + filename_save,
+                    include_plotlyjs = True, include_mathjax = False)
--- scripts/src/package/heatmap/__init__.py
+++ scripts/src/package/heatmap/__init__.py
@@ -1,2 +1,4 @@
 from .data_handler import *
 from .data_plotter import *
+
+from .Heatmap import Heatmap
